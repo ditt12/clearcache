affe6aab9315c28e95289f4c0dbe975255810ad6
asyraff <145573395+ditt12@users.noreply.github.com>
2024-12-05 10:50:06 +0700

    Update

---
+++ nonrootfs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Mengecek aplikasi yang berjalan di latar belakang..."
+
+APPS=$(adb shell "pm list packages -3" | awk -F: '{print $2}')
+
+for APP in $APPS; do
+  adb shell "am force-stop $APP"
+  echo "Menghentikan aplikasi: $APP"
+done
+
+echo "Semua aplikasi latar belakang telah dihentikan."
