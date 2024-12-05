2eaaa27725ceaa7ca16db2b19c9b6ca7a416af2a
asyraff <145573395+ditt12@users.noreply.github.com>
2024-12-05 10:51:00 +0700

    Update

---
+++ rootfs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Root akses diperlukan. Jalankan script ini dengan akses root."
+  exit 1
+fi
+
+echo "Mengecek semua proses yang berjalan..."
+
+PIDS=$(ps -A | awk '{print $1}' | tail -n +2)  
+
+for PID in $PIDS; do
+  kill -9 $PID
+  echo "Menghentikan proses dengan PID: $PID"
+done
+
+echo "Semua proses latar belakang telah dihentikan."
